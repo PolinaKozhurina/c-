6e7c378bf316d9c3c6fe21146ca1365d4cd9c275
PolinaKozhurina <90439685+PolinaKozhurina@users.noreply.github.com>
2021-11-03 22:25:38 +0300

    Update rectangle.cpp

--- rectangle.cpp
+++ rectangle.cpp
@@ -1,71 +1,74 @@
-#include <iostream>
+#include <string>
 #include <fstream>
+#include <iostream>
 #include <cstdlib>
 using namespace std;
 
 class Rectangle {
 private:
+    string sxL, sxR, syD, syU;
     double xL, xR, yD, yU;
+
 public:
-    Rectangle(double a, double b, double c, double d) : xR(a), xL(b), yD(c), yU(d)	{	}
-    void Rectangle::Input() {
+    Rectangle(double a, double b, double c, double d) : xR(a), xL(b), yD(c), yU(d) {	}
+    void Input() {
         ifstream inf("date.txt");
         if (!inf)
         {
             cerr << "Uh oh, date.txt could not be opened for reading!" << endl;
             exit(1);
         }
-        cout << "Enter the x coordinate of the left points" << endl;
-        getline(inf,xL);
-        cout << "Enter the y coordinate of the bottom points" << endl;
-        cin >> yD;
-        cout << "Enter the x coordinate of the right points" << endl;
-        cin >> xR;
-        cout << "Enter the y coordinate of the top points" << endl;
-        cin >> yU;
+        getline(inf, sxL);
+        xL=stod(sxL);
+        getline(inf, syD);
+        yD = stod(syD);
+        getline(inf, sxR);
+        xR = stod(sxR);
+        getline(inf, syU);
+        yU = stod(syU);
     }
-    void Rectangle::Result() {
+    void Result() {
         cout << "You entered the following coordinates of the rectangle:" << endl;
         cout << " A:" << "(" << xL << "," << yD << ")" << endl;
         cout << " B:" << "(" << xR << "," << yD << ")" << endl;
         cout << " C:" << "(" << xR << "," << yU << ")" << endl;
         cout << " D:" << "(" << xL << "," << yU << ")" << endl;
     }
-    void Rectangle::Crossing(Rectangle rec2) {
-        double x_min = xL, x_max = xR, y_min = yD,y_max = yU;
-        double A[4]= {xR,xL,rec2.xL,rec2.xR}, B[4]= {yD,yU,rec2.yD,rec2.yU};
-        double S,S_min;
-        for(int i = 0; i < 4; i ++)
+    void Crossing(Rectangle rec2) {
+        double x_min = xL, x_max = xR, y_min = yD, y_max = yU;
+        double A[4] = { xR,xL,rec2.xL,rec2.xR }, B[4] = { yD,yU,rec2.yD,rec2.yU };
+        double S, S_min;
+        for (int i = 0; i < 4; i++)
         {
-            if(x_max < A[i])
+            if (x_max < A[i])
             {
                 x_max = A[i];
             }
-            if(y_max < B[i])
+            if (y_max < B[i])
             {
                 y_max = B[i];
             }
-            if(x_min > A[i])
+            if (x_min > A[i])
             {
                 x_min = A[i];
             }
-            if(y_min > B[i])
+            if (y_min > B[i])
             {
                 y_min = B[i];
             }
         }
-        S_min=((x_max-x_min)*(y_max-y_min));
-        S = ((xR-xL)+(rec2.xR-rec2.xL))*((yU-yD)+(rec2.yU-rec2.yD));
-        if (S_min<=S)
-            cout <<"They don't intersect";
-        else cout <<"They intersect";
+        S_min = ((x_max - x_min) * (y_max - y_min));
+        S = ((xR - xL) + (rec2.xR - rec2.xL)) * ((yU - yD) + (rec2.yU - rec2.yD));
+        if (S_min <S)
+            cout << "They don't intersect";
+        else cout << "They intersect";
     }
 };
 int main(void)
-{ Rectangle r1(1,2,3,4), r2(1,2,3,4);
+{
+    Rectangle r1(1, 2, 3, 4), r2(2, 3, 4, 5);
     r1.Input();
     r1.Result();
-    r2.Input();
     r2.Result();
     r1.Crossing(r2);
 }
